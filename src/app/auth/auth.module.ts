9d1fe4ed13844cb9c47ddaa4d542a5bb9515a8a7
Mr-Knister <cesar.gomez.maquera@gmail.com>
2023-06-23 10:01:03 -0500

    end class 14

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MaterialModule } from '../material/material.module';
+
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
@@ -11,11 +13,12 @@ import { AuthhRoutingModule } from './auth-routing.module';
   declarations: [
     LayoutPageComponent,
     LoginPageComponent,
-    RegisterPageComponent
+    RegisterPageComponent,
   ],
   imports: [
     CommonModule,
-    AuthhRoutingModule
+    AuthhRoutingModule,
+    MaterialModule,
   ]
 })
 export class AuthModule { }
